58a707a3caffe5a54577b7d0cd81443cdb278f48
Laksh Jaisinghani <jaisinghanilaksh@gmai.com>
2020-08-12 15:38:25 +1000

    added keyb

--- Week01-02/keyboardControlStarter.py
+++ Week01-02/keyboardControlStarter.py
@@ -6,11 +6,14 @@ import cv2
 import numpy as np
 
 class Keyboard:
-    # feel free to change the speed, or add keys to do so
-    wheel_vel_forward = 100
-    wheel_vel_rotation = 20
+    
+    
 
     def __init__(self, ppi=None):
+        # feel free to change the speed, or add keys to do so
+        self. wheel_vel_forward = 100
+        self.wheel_vel_rotation = 20
+        
         # storage for key presses
         self.directions = [False for _ in range(4)]
         self.signal_stop = False 
@@ -26,36 +29,55 @@ class Keyboard:
         # use arrow keys to drive, space key to stop
         # feel free to add more keys
         if key == Key.up:
-            self.directions[0] = True
+            self.directions = [True, False, False, False]
+
         elif key == Key.down:
-            self.directions[1] = True
+            self.directions = [False, True, False, False]
+
         elif key == Key.left:
             self.directions[2] = True
+            self.directions[3] = False
+
         elif key == Key.right:
             self.directions[3] = True
+            self.directions[2] = False
+
         elif key == Key.space:
-            self.signal_stop = True
+            self.directions[:] = [False, False, False, False]
 
         self.send_drive_signal()
         
-    def get_drive_signal(self):           
-        # translate the key presses into drive signals 
-        
-        # compute drive_forward and drive_rotate using wheel_vel_forward and wheel_vel_rotation
-        # drive_forward = ???
-        # drive_rotate = ???
+    def get_drive_signal(self):  
+        left_speed  = 0
+        right_speed = 0
 
-        # translate drive_forward and drive_rotate into left_speed and right_speed
-        # left_speed = ???
-        # right_speed = ???
+        # translate the key presses into drive signals
+        if self.directions[0]:
+            left_speed  = self.wheel_vel_forward
+            right_speed = self.wheel_vel_forward
+            
+        if self.directions[1]:
+            left_speed  = -self.wheel_vel_forward
+            right_speed = -self.wheel_vel_forward
+
+        if self.directions[2]:
+            left_speed  += 0
+            right_speed += self.wheel_vel_rotation
+
+        if self.directions[3]:
+            left_speed  += self.wheel_vel_rotation
+            right_speed += 0
 
         return left_speed, right_speed
+        
     
     def send_drive_signal(self):
         if not self.ppi is None:
             lv, rv = self.get_drive_signal()
             lv, rv = self.ppi.set_velocity(lv, rv)
             self.wheel_vels = [lv, rv]
+            print(self.wheel_vels)
+            print(self.directions)
             
     def latest_drive_signal(self):
         return self.wheel_vels
@@ -79,7 +101,7 @@ if __name__ == "__main__":
         line_type = 2
 
         # get velocity of each wheel
-        wheel_vels = keyboard_control.latest_drive_signal();
+        wheel_vels = keyboard_control.latest_drive_signal()
         L_Wvel = wheel_vels[0]
         R_Wvel = wheel_vels[1]
 
